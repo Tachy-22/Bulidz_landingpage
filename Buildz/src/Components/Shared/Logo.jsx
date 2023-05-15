e85bda233369df815c5b2f01c3f2de32ac9825ad
Tachy-22 <entekumejeffrey@gmail.com>
2023-05-15 12:36:47 +0100

    update test

--- Buildz/src/Components/Shared/Logo.jsx
+++ Buildz/src/Components/Shared/Logo.jsx
@@ -2,7 +2,7 @@ function Logo() {
   return (
     <div className="w-fit px-3 text-5xl logo h-fit ">
       <h1 className="capitalize font-bold ">
-        B<span className=" font-medium">uildz</span>
+        B<span className=" font-medium ">uildz</span>
       </h1>
     </div>
   );
